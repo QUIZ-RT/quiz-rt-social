534624f1ce8dc723d6a289a7e75a39ac3071c26b
Prashant Shrivastava <prashriv2@publicisgroupe.net>
2018-08-02 17:27:35 +0530

    login file change

--- src/client/app.js
+++ src/client/app.js
@@ -7,6 +7,7 @@ import {createMenu} from "./ui-component/menu/menu.controller"
 import {createMainContainer} from "./ui-component/main/main.controlller"
 import {createPopularTopicSection, createFavoriteTopicSection, createChallengesSection, createMyChallengesSection} from "./ui-component/dashboard/dashboard.controller"
 import {createTopicmodal} from "./ui-component/topic-modal/topic-modal.controller"
+//import {createUserLogin} from "./ui-component/login-page/login.controller"
 //import {GoogleLogin} from "./ui-component/login-page/login.controller"
 //GoogleLogin()
 createHeader()
