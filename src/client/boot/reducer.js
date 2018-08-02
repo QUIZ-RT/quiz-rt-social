beb2c026526c775a0dd97a43d42b32759c19d5e4
KirtiBihari <bihari.kirti@gmail.com>
2018-08-02 22:23:12 +0530

    dashboard and menu redux code

--- src/client/boot/reducer.js
+++ src/client/boot/reducer.js
@@ -1,19 +1,9 @@
 import {combineReducers} from "redux"
-import {menuReducer} from "../ui-component/menu/menu.redux"
-<<<<<<< HEAD
-// import {dashboardReducer} from "../ui-component/dashboard/dashboard.redux"
-
-=======
-import {topicReducer} from "../ui-component/topics/topic.reducer"
->>>>>>> 88195b282cc1e90564984e49e325936e857545a8
+// import {menuReducer} from "../ui-component/menu/menu.redux"
+// import {topicReducer} from "../ui-component/topics/topic.reducer"
 // Combined all the reducers
 
 export default combineReducers({
-  menuReducer,
-<<<<<<< HEAD
-//   dashboardReducer,
-
-=======
-  topicReducer,
->>>>>>> 88195b282cc1e90564984e49e325936e857545a8
+//   menuReducer,
+//   topicReducer,
 })
