0f7731ad6a0d4a729db017af2e364a661715da78
Ranjit Jena <ranjena@publicisgroupe.net>
2018-08-06 14:36:58 +0530

    css issue fixed

--- src/client/ui-component/topics/topic.view.js
+++ src/client/ui-component/topics/topic.view.js
@@ -5,13 +5,17 @@ export const htmlToTemplate = (htmlstr) => {
   }
 
 export const topicView = (topicObj, topicId) => {
+  let userNum =  '';
+  if(topicObj.users!==undefined){
+      userNum = topicObj.users.length;
+  } 
   return `<li class="mdc-grid-tile pointer" id="grid_${topicId}">
     <div class="mdc-grid-tile__primary">
         <img class="mdc-grid-tile__primary-content mdc-image-list__image" src="${topicObj.topicImage}" />
         
     </div>
     <span class="mdc-grid-tile__secondary">
-        <span class="mdc-grid-tile__title">${topicObj.topicText} </span>
+        <span class="mdc-grid-tile__title">${topicObj.topicText} &nbsp;&nbsp;${userNum} </span>
     </span>
 </li>`
 }
