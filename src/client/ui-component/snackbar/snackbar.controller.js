d060e12b40c1397ea396cbf425fe66faf0ca8012
KirtiBihari <bihari.kirti@gmail.com>
2018-08-06 10:45:19 +0530

    added snackbar

---
+++ src/client/ui-component/snackbar/snackbar.controller.js
@@ -0,0 +1,21 @@
+import {getSnackBarHtml, renderViewToContainer} from "./snackbar.view"
+
+export const createSnackBar = () =>{
+    const snackBarTemplate = getSnackBarHtml();
+    renderViewToContainer(snackBarTemplate, "body")
+}
+
+export const showSnackBar = (text, msgtype) => {
+    const snackBar = document.querySelector("div#quiz-snackbar")
+    snackBar.innerHTML = text
+    snackBar.style.display = "block"
+    if (msgtype.toLowerCase() === "success") {
+      snackBar.style.background = "#06a00c"
+    }
+    else {
+      snackBar.style.background = "#e2291b"
+    }
+    setTimeout(function() {
+      snackBar.style.display = "none"
+    }, 3000)
+}
