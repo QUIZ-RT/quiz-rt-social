620b78ff498732f950c494866893e2cddf20a7e8
prshri <40630094+prshri@users.noreply.github.com>
2018-08-06 19:52:23 +0530

    Update menu.controller.js

--- src/client/ui-component/menu/menu.controller.js
+++ src/client/ui-component/menu/menu.controller.js
@@ -51,9 +51,9 @@ export const createMenu = () => {
     })
   })
   renderViewToContainer(menuContent, "#quiz-header")
-  const emailName = document.getElementById("loggedInEmail")
-  const currentState = Store.getState()
-  emailName.innerText = currentState.menuReducer.currentUserInfo.email;
+  const currentState = Store.getState();
+  //console.log("currentState = ", currentState.menuReducer.currentUserInfo.email)
+  document.getElementById("loggedInEmail").innerText = currentState.menuReducer.currentUserInfo.email;
   const drawerEl = document.querySelector(".mdc-drawer")
   drawer = new MDCTemporaryDrawer(drawerEl)
   document.querySelector(".sidemenu").addEventListener("click", function() {
