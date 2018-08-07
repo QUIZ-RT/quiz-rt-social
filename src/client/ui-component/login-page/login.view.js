fbea3b499ce0765f5af21dbd2b3dc42112884e1b
Vinita <vinni.smile26@gmail.com>
2018-08-07 13:55:19 +0000

    changed login page look

--- src/client/ui-component/login-page/login.view.js
+++ src/client/ui-component/login-page/login.view.js
@@ -7,22 +7,11 @@ const htmlToTemplate = (htmlstr) => {
 // first
 export const getUserTemplate = () => {
   const userHtmlContent = `
-  <div class="mdl-layout mdl-js-layout">
-  <main class="mdl-layout__content" style = "margin: auto">
-  <div class="mdl-card mdl-shadow--6dp">
-  <div class="mdl-card__title mdl-color--primary mdl-color-text--white">
-  <h6 class="mdl-card__title-text">Quiz RT Login</h6>
-  <!-- <div style="text-align: right; width: 20%;">
-  <img src = "src/client/assets/google-logo.png" style="width:40%" />
-  </div> -->
   </div>
-  <div class="mdl-card__supporting-text" style="padding:20px">
+  <div class="mdl-card__supporting-text login-hero-img" style="padding:20px">
   <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" id="signin">
   Signin  With  Google 
   </button> 
-  </div>
-  </div>
-  </main>
   </div>
   `
 
