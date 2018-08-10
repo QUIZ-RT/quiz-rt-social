6f3e98dab39744c2b41068a92e7511035b4e34f2
unknown <sanpadhy@WKWIN8244532.global.publicisgroupe.net>
2018-08-10 16:58:55 +0530

    leader board changes

--- src/client/ui-component/leader-board/service-methods.js
+++ src/client/ui-component/leader-board/service-methods.js
@@ -3,12 +3,17 @@ export const serviceCall = (url) => {
     return fetch(url, {
       method: "GET",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
         //"Cache-Control": "no-cache",
-      }  
+        "Access-Control-Allow-Origin" : "*",        
+        "Access-Control-Expose-Headers": "Content-Length, X-JSON",
+        "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "*"
+      }
     }).then(
       res => res.json()
     ).then(function(response) {       
+      debugger;
       return response;
     }).catch(function(error) {
       console.log("Request failure: ", error)
